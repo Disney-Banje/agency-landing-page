bdd03070ddbe4ec040ccb8e62c179ab0ad6077d1
Disney <disneyconstant996@gmail.com>
2024-07-28 17:20:13 +0300

    Refactor code, destructure props objects & improve code readability

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,8 @@
 
 
 
-function Button(props) {
-    if (props.state) {
+function Button({ state }) {
+    if (state) {
         return <button className="font-fraunces font-bold uppercase bg-white text-heading-text-color py-1 px-3 rounded-3xl text-base hover:bg-[#6fcfff] hover:text-white">contact</button>
     }
     return <button className="font-fraunces font-bold uppercase bg-link-yellow text-heading-text-color py-1 px-3 rounded-3xl text-base">contact</button>
