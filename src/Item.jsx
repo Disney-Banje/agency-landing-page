11cf4d3916b1fb75e03a3150eb211e49468dae2c
Disney <disneyconstant996@gmail.com>
2024-07-28 17:33:05 +0300

    Add animation for the underline

--- src/Item.jsx
+++ src/Item.jsx
@@ -16,11 +16,11 @@ function Items({ cover, cover1, title, summary, color, cta }) {
                         <p className="text-dark-grayish-blue font-semibold mb-5">{summary}</p>
                         {/* Ternary operator for pseudo element since they have different colors. */}
                         {color === 'yellow' ? (
-                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-yellow after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100">
+                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-yellow after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100 hover:after:-rotate-3">
                                     <span className="relative">{cta}</span>
                                 </a>
                             ) : (
-                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-soft-red after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100">
+                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-soft-red after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100 hover:after:-rotate-3">
                                     <span className="relative">{cta}</span>
                                 </a>
                             )}
@@ -30,11 +30,11 @@ function Items({ cover, cover1, title, summary, color, cta }) {
                         <h2 className="font-fraunces text-4xl xl:text-5xl font-extrabold text-heading-text-color mb-5">{title}</h2>
                         <p className="text-dark-grayish-blue font-semibold mb-5">{summary}</p>
                         {color === 'yellow' ? (
-                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-yellow after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100">
+                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-yellow after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100 hover:after:-rotate-3">
                                     <span className="relative">{cta}</span>
                                 </a>
                             ) : (
-                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-soft-red after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100">
+                                <a href="#" className="text-heading-text-color font-fraunces font-bold text-xl relative px-2 after:absolute after:w-full after:h-2 after:bg-link-soft-red after:left-0 after:top-4 after:rounded-3xl after:-z-10 after:opacity-50 hover:after:opacity-100 hover:after:-rotate-3">
                                     <span className="relative">{cta}</span>
                                 </a>
                             )}
